cc5db3eb981b67b7c43d6a277b209642ace4eca6
esteng <elias@gouda.linguistics.mcgill.ca>
2016-11-10 12:23:50 -0500

    cr and uk

---
+++ Croatian/CroatianEnrichment.py
@@ -0,0 +1,78 @@
+import re
+import csv
+import urllib.request
+
+"""
+Frequency info from https://github.com/hermitdave/FrequencyWords/
+
+word type information gotten from wiktionary (https://en.wiktionary.org/wiki/Category:Bulgarian_lemmas)
+
+pronouns, prepositions, and conjunctions are function words, all others are content
+"""
+
+words = {}
+def getHTML(page):
+    """ get raw html from page"""
+    with urllib.request.urlopen(page) as response:
+        html = response.read().decode('utf-8')
+    return html
+
+def get_function_words():
+	functionwords = []
+	conjhtml = str(getHTML('https://en.wiktionary.org/wiki/Category:Serbo-Croatian_conjunctions'))
+	prephtml = str(getHTML('https://en.wiktionary.org/wiki/Category:Serbo-Croatian_prepositions'))
+	pronhtml = str(getHTML('https://en.wiktionary.org/wiki/Category:Serbo-Croatian_pronouns'))
+
+	regex = re.compile('<li><a href="\/wiki\/.*?" title=".*?">.*?<\/a><\/li>')
+
+	conjmatches = regex.findall(conjhtml)
+	prepmatches = regex.findall(prephtml)
+	pronmatches = regex.findall(pronhtml)
+
+	wordregex = re.compile("\">.+?<")
+
+	for match in (conjmatches+pronmatches+prepmatches):
+		rmatch = wordregex.search(match).group(0)
+		if("којешта" in rmatch):
+			print(match)
+		functionwords.append(rmatch[2:-1])
+
+
+	for word in functionwords:
+		if 'Croatian' in word or 'lemmas' in word or 'by' in word or word == 'Prepositions by language':
+			functionwords.remove(word)
+
+
+	return(set(functionwords))
+
+
+
+
+def getFrequency(path):
+	with open(path,errors='ignore') as f:
+		lines = f.readlines()
+	total = sum([int(re.split('\s', x)[1]) for x in lines])
+
+	for line in lines:
+		splitline = re.split('\s', line)
+		
+		word = splitline[0]
+		
+		freq = (int(splitline[1])/total)*1000000
+
+		
+		words[word] = freq
+
+fwords= get_function_words()
+getFrequency('sr_full.txt')
+f2 = open('CroatianEnrichmentData.csv', 'w')
+f2CW = csv.writer(f2)
+f2CW.writerow(['word','word type','frequency'])
+
+
+for k,v in words.items():
+	if k in fwords:
+		wordtype = 'function'
+	else:
+		wordtype = 'content'
+	f2CW.writerow([k,wordtype, v])
---
+++ Ukrainian/UkrainianEnrichment.py
@@ -0,0 +1,76 @@
+import re
+import csv
+import urllib.request
+
+"""
+Frequency info from https://github.com/hermitdave/FrequencyWords/
+
+word type information gotten from wiktionary (https://en.wiktionary.org/wiki/Category:Bulgarian_lemmas)
+
+pronouns, prepositions, and conjunctions are function words, all others are content
+"""
+
+words = {}
+def getHTML(page):
+    """ get raw html from page"""
+    with urllib.request.urlopen(page) as response:
+        html = response.read().decode('utf-8')
+    return html
+
+def get_function_words():
+	functionwords = []
+	conjhtml = str(getHTML('https://en.wiktionary.org/wiki/Category:Ukrainian_conjunctions'))
+	prephtml = str(getHTML('https://en.wiktionary.org/wiki/Category:Ukrainian_prepositions'))
+	pronhtml = str(getHTML('https://en.wiktionary.org/wiki/Category:Ukrainian_pronouns'))
+
+	regex = re.compile('<li><a href="\/wiki\/.*?" title=".*?">.*?<\/a><\/li>')
+
+	conjmatches = regex.findall(conjhtml)
+	prepmatches = regex.findall(prephtml)
+	pronmatches = regex.findall(pronhtml)
+
+	wordregex = re.compile("\">.+?<")
+
+	for match in (conjmatches+pronmatches+prepmatches):
+		rmatch = wordregex.search(match).group(0)
+		functionwords.append(rmatch[2:-1])
+
+
+	for word in functionwords:
+		if 'Ukrainian' in word or 'lemmas' in word or 'by' in word or word == 'Prepositions by language':
+			functionwords.remove(word)
+
+
+	return(set(functionwords))
+
+
+
+
+def getFrequency(path):
+	with open(path,errors='ignore') as f:
+		lines = f.readlines()
+	total = sum([int(re.split('\s', x)[1]) for x in lines])
+
+	for line in lines:
+		splitline = re.split('\s', line)
+		
+		word = splitline[0]
+		
+		freq = (int(splitline[1])/total)*1000000
+
+		
+		words[word] = freq
+
+fwords= get_function_words()
+getFrequency('uk_full.txt')
+f2 = open('UkrainianEnrichmentData.csv', 'w')
+f2CW = csv.writer(f2)
+f2CW.writerow(['word','word type','frequency'])
+
+
+for k,v in words.items():
+	if k in fwords:
+		wordtype = 'function'
+	else:
+		wordtype = 'content'
+	f2CW.writerow([k,wordtype, v])
