9115e580062e9fe38b391a568f266821f7bd8d6e
esteng <elias@gouda.linguistics.mcgill.ca>
2017-01-27 11:46:20 -0500

    working scraper for ukrainian

--- Ukrainian/dictua_scraper_two.py
+++ Ukrainian/dictua_scraper_two.py
@@ -1,37 +1,122 @@
 import re
-import mechanize
+import requests
+from urllib.request import FancyURLopener
+import http.client
+from bs4 import BeautifulSoup
+from collections import defaultdict
+from time import sleep
+ 
+
+HEADERS = {
+"Host": "lcorp.ulif.org.ua",
+"Connection": "keep-alive",
+"Content-Length": "10901",
+"Cache-Control": "no-cache",
+"Origin": "http://lcorp.ulif.org.ua",
+"X-MicrosoftAjax": "Delta=true",
+"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36",
+"Content-Type": "application/x-www-form-urlencoded",
+"Accept": "*/*",
+"Referer": "http://lcorp.ulif.org.ua/dictua/",
+"Accept-Encoding": "gzip, deflate",
+"Accept-Language": "en-US,en;q=0.8",
+"Cookie": "SESS86f0a93a60b489e31e9d907b879bdad1=avr2c3uc42jtckdt0tnv64gpb7; ASP.NET_SessionId=mmaz0xvj3fhd0045soyqiy45"
+}
 
 
+vowels = ["а", "е", "є", "и", "і", "ї", "о", "у", "ю", "я"]
+ord_vowels = [ord(x) for x in vowels]
+
+class MyOpener(FancyURLopener):
+    version = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.57 Safari/537.17'
+
+myopener = MyOpener()
+url = "http://lcorp.ulif.org.ua/dictua/"
+
+f=  myopener.open(url)
+soup = BeautifulSoup(f,'lxml')
+
+stress_dict = defaultdict()
+
+viewstate = soup.select("#__VIEWSTATE")[0]['value']
 
 
+def get_word_html(word):
+
+    pay = {"ctl00$ContentPlaceHolder1$ScriptManager1":"ctl00$ContentPlaceHolder1$UpdText|ctl00$ContentPlaceHolder1$search",
+    "__EVENTTARGET":"ctl00$ContentPlaceHolder1$dgv",
+    "__EVENTARGUMENT":"Select$21",
+    "__VIEWSTATE" : viewstate,
+    "__VIEWSTATEGENERATOR": soup.select("#__VIEWSTATEGENERATOR")[0]['value'],
+    "__EVENTVALIDATION": soup.select("#__EVENTVALIDATION")[0]['value'],
+    "ctl00$ContentPlaceHolder1$tsearch": word,
+    "ctl00$ContentPlaceHolder1$search.x":"29",
+    "ctl00$ContentPlaceHolder1$search.y":"13"}
+
+    r = requests.post(url, data = pay, headers = HEADERS)
+
+    return r.text
+
+def parse_html(html):
+    all_words = set()
+    spanregex = "(<span.*>)|(<\/span>)"
+    tdregex = "(<td.*>)|(<\/td>)"
+
+    soup = BeautifulSoup(html,'lxml')
+
+    word_trans_list = soup.find_all("span",'word_style')
+    declension_trans_list = soup.find_all("td","td_inner_style")
+
+    for span in word_trans_list:
+        nospan = re.sub(spanregex, "", str(span))
+        splitup = re.split(',', nospan)
+        splitup = [x.strip() for x in splitup]
+        all_words |= set(splitup)
+    for td in declension_trans_list:
+        notd = re.sub(tdregex, "", str(td))
+        tdsplitup = re.split(',', notd)
+        tdsplitup = [x.strip() for x in tdsplitup]
+        all_words |= set(tdsplitup)
+    
+    just_words = list(all_words)
+    return just_words
 
 
-URL = "http://lcorp.ulif.org.ua/dictua/"
+def make_stress_pattern(word, stressed):
+    pattern = ""
+    for j, char in enumerate(word):
+        if char in vowels and j!= stressed:
+            pattern +='0'
+        elif char in vowels and j == stressed:
+            pattern += '1'
+    return pattern
 
-br = mechanize.Browser()
-br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
-response = br.open(URL)
+def get_stress(words):
+    for word in words:
+        if word!='':
+            for i, char in enumerate(word):
+                if char == u"\u0301":
+                    stressed = i-1
+                    word = re.sub(u"\u0301", "", word)
+                    stress_pattern = make_stress_pattern(word, stressed)
+                    # print("adding ", word, ' : ', stress_pattern)
+                    stress_dict[word] = stress_pattern
 
 
+def get_all_words():
+    with open('uk_full.txt') as f1:
+        lines = f1.readlines()
+    words = [re.split("\s", x)[0].strip() for x in lines]
+    for i,word in enumerate(words):
+        if i%100 == 0:
+            print("{} % done".format(i/len(words)*100))
+        try:
+            get_stress(parse_html(get_word_html(word)))
+            sleep(.1)
+        except:
+            pass
 
-for i in range(10):
-    html = response.read()
-    # print("Page %d :" % i, html)
-
-    br.select_form(nr=0)
-    # print( br.form)
-    br.set_all_readonly(False)
-
-    with open("test.html",'w') as f2:
-        f2.write(html)
-
-    mnext = re.search("""<a href="javascript:__doPostBack('ctl00\$ContentPlaceHolder1\$dgv','Select\$\d+)'""", html)
-    if not mnext:
-        print("not found")
-        break
-    br["__EVENTTARGET"] = mnext.group(1)
-    br["__EVENTARGUMENT"] = mnext.group(2)
-    br.find_control("btnSearch").disabled = True
-    response = br.submit()
-    print(response)
-
+get_all_words()
+with open("stresses_from_dictua.txt",'w') as f3:
+    for key, entry in stress_dict.items():
+        f3.write(key, " ", entry, "\n")
